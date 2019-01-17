c3c40eee33087fc691e8045067eb6896cd10ac0a
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-17 09:05:46 +0100

    skip for now

--- src/main/java/no/nav/foreldrepenger/lookup/ws/joark/JoarkConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/lookup/ws/joark/JoarkConfiguration.java
@@ -17,34 +17,34 @@ import no.nav.tjeneste.virksomhet.innsynjournal.v2.binding.InnsynJournalV2;
 //@Configuration
 public class JoarkConfiguration extends WsClient<InnsynJournalV2> {
 
-    private static final String JOARK_V2 = "joarkV2";
-    private static final String HEALTH_INDICATOR_JOARK = "healthIndicatorJoarkV2";
-    private static final String JOARK_V2RETRY = "joarkV2retry";
+    private static final String JOARK = "joark";
+    private static final String HEALTH_INDICATOR = "healthIndicator" + JOARK;
+    private static final String RETRY = "retry" + JOARK;
 
     @Bean
-    @Qualifier(JOARK_V2)
+    @Qualifier(JOARK)
     public InnsynJournalV2 joarkV2(
             @Value("${JOURNAL_V2_ENDPOINTURL}") String serviceUrl) {
         return createPortForExternalUser(serviceUrl, InnsynJournalV2.class);
     }
 
     @Bean
-    @Qualifier(HEALTH_INDICATOR_JOARK)
+    @Qualifier(HEALTH_INDICATOR)
     public InnsynJournalV2 healthIndicatorJoark(
             @Value("${JOURNAL_V2_ENDPOINTURL}") String serviceUrl) {
         return createPortForSystemUser(serviceUrl, InnsynJournalV2.class);
     }
 
     @Bean
-    public JoarkClient joarkClientWs(@Qualifier(JOARK_V2) InnsynJournalV2 joarkV2,
-            @Qualifier(HEALTH_INDICATOR_JOARK) InnsynJournalV2 healthIndicator, TokenUtil tokenHandler,
-            @Qualifier(JOARK_V2RETRY) Retry retry) {
+    public JoarkClient joarkClientWs(@Qualifier(JOARK) InnsynJournalV2 joarkV2,
+            @Qualifier(HEALTH_INDICATOR) InnsynJournalV2 healthIndicator, TokenUtil tokenHandler,
+            @Qualifier(RETRY) Retry retry) {
         return new JoarkClientWs(joarkV2, healthIndicator, tokenHandler, retry);
     }
 
     @Bean
-    @Qualifier(JOARK_V2RETRY)
+    @Qualifier(RETRY)
     public Retry joarketryConfig(@Value("${retry.joark.max:2}") int max) {
-        return retry(max, "joark", SOAPFaultException.class, getLogger(JoarkClientWs.class));
+        return retry(max, JOARK, SOAPFaultException.class, getLogger(JoarkClientWs.class));
     }
 }
