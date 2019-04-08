ea37fa8ab50604ecbf9bd5584ea65dd443bbb3b5
Jan-Olav Eide <janolaveide@gmail.com>
2019-04-08 13:50:40 +0200

    hent navn

--- src/test/java/no/nav/foreldrepenger/stub/PersonClientStub.java
+++ src/test/java/no/nav/foreldrepenger/stub/PersonClientStub.java
@@ -20,8 +20,8 @@ public class PersonClientStub implements PersonClient {
     public Person hentPersonInfo(ID id) {
         Navn navn = new Navn("Skjegg", "Stub", "Sveen");
         return new Person(id, CountryCode.NO, Kjønn.valueOf("M"), navn,
-            "NN", new Bankkonto("1234567890", "Stub NOR"),
-            now().minusYears(20), barn(id.getFnr()));
+                "NN", new Bankkonto("1234567890", "Stub NOR"),
+                now().minusYears(20), barn(id.getFnr()));
     }
 
     @Override
@@ -32,12 +32,18 @@ public class PersonClientStub implements PersonClient {
 
     private List<Barn> barn(Fødselsnummer fnrMor) {
         Barn barn = new Barn(fnrMor,
-            new Fødselsnummer("01011812345"),
-            now().minusYears(1),
-            new Navn("Mo", null, "Sveen"),
-            Kjønn.M,
-            new AnnenForelder(new Navn("Anne", "N", "Forelder"), new Fødselsnummer("01019012345"), now().minusYears(25)));
+                new Fødselsnummer("01011812345"),
+                now().minusYears(1),
+                new Navn("Mo", null, "Sveen"),
+                Kjønn.M,
+                new AnnenForelder(new Navn("Anne", "N", "Forelder"), new Fødselsnummer("01019012345"),
+                        now().minusYears(25)));
 
         return singletonList(barn);
+    }
+
+    @Override
+    public Navn navn(Fødselsnummer fnr) {
+      return new Navn("Ole", "Gunnar", "Solskjær")
     }
 }
