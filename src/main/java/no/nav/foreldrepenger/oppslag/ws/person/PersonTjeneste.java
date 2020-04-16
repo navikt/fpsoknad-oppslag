21b2d3f7d9aa52011b18b9716ee0d0497e5bce6f
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-04-16 13:00:41 +0200

    ingen retry på not found og unauthorized

--- src/main/java/no/nav/foreldrepenger/oppslag/ws/person/PersonTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/ws/person/PersonTjeneste.java
@@ -4,10 +4,13 @@ import javax.xml.ws.soap.SOAPFaultException;
 
 import org.springframework.retry.annotation.Retryable;
 
+import no.nav.foreldrepenger.oppslag.error.NotFoundException;
 import no.nav.foreldrepenger.oppslag.error.TokenExpiredException;
+import no.nav.foreldrepenger.oppslag.error.UnauthorizedException;
 import no.nav.foreldrepenger.oppslag.util.Pingable;
 
-@Retryable(include = SOAPFaultException.class, exclude = TokenExpiredException.class)
+@Retryable(include = SOAPFaultException.class, exclude = { UnauthorizedException.class, NotFoundException.class,
+        TokenExpiredException.class })
 public interface PersonTjeneste extends Pingable {
 
     Person hentPersonInfo(ID id);
