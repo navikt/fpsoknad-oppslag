f829422692387bc5d1218b5000462115519a807a
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-05-25 15:22:57 +0200

    first short rest

--- src/main/java/no/nav/foreldrepenger/oppslag/rest/filters/SystemUserTokenService.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/rest/filters/SystemUserTokenService.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.oppslag.rest.filters;
 
+import java.net.URI;
 import java.util.Base64;
 
 import org.slf4j.Logger;
@@ -11,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class SystemUserTokenService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SystemUserTokenService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SystemUserTokenService.class);
 
     private final RestTemplate restTemplate;
 
@@ -19,34 +20,38 @@ public class SystemUserTokenService {
 
     private final String password;
 
+    private final URI uri;
+
     public SystemUserTokenService(RestTemplate restTemplate, @Value("${FPSELVBETJENING_USERNAME}") String username,
-            @Value("${FPSELVBETJENING_PASSWORD}") String password) {
+            @Value("${FPSELVBETJENING_PASSWORD}") String password,
+            @Value("${sts.uri}") URI uri) {
         this.restTemplate = restTemplate;
         this.username = username;
         this.password = password;
-        LOGGER.info("username " + username);
-
+        this.uri = uri;
+        LOG.info("STS uri er" + uri);
     }
 
-    public UserToken fetch() {
-        return null;
-        /*
-         * LOGGER.trace("Retrieving JWT token for service user"); HttpHeaders headers =
-         * new HttpHeaders(); headers.add(HttpHeaders.AUTHORIZATION,
-         * getBasicAuthHeader()); headers.setContentType(MediaType.APPLICATION_JSON);
-         * 
-         * HttpEntity restRequest = new HttpEntity<>(headers);
-         * 
-         * UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUri(uri)
-         * .path("/rest/v1/sts/token") .queryParam("grant_type", "client_credentials")
-         * .queryParam("scope", "openid");
-         * 
-         * try { return restTemplate.exchange( uriBuilder.toUriString(), HttpMethod.GET,
-         * restRequest, UserToken.class).getBody(); } catch (HttpStatusCodeException e)
-         * { throw new RuntimeException("Feil ved henting av SystembrukerToken: " +
-         * e.getMessage(), e); }
-         */
-    }
+    /*
+     * public UserToken fetch() { return null;
+     * 
+     * LOGGER.trace("Retrieving JWT token for service user"); HttpHeaders headers =
+     * new HttpHeaders(); headers.add(HttpHeaders.AUTHORIZATION,
+     * getBasicAuthHeader()); headers.setContentType(MediaType.APPLICATION_JSON);
+     * 
+     * HttpEntity restRequest = new HttpEntity<>(headers);
+     * 
+     * UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUri(uri)
+     * .path("/rest/v1/sts/token").queryParam("grant_type", "client_credentials")
+     * .queryParam("scope", "openid");
+     * 
+     * try { return restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.GET,
+     * restRequest, UserToken.class).getBody(); } catch (HttpStatusCodeException e)
+     * { throw new RuntimeException("Feil ved henting av SystembrukerToken: " +
+     * e.getMessage(), e); }
+     * 
+     * }
+     */
 
     private String getBasicAuthHeader() {
         return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
