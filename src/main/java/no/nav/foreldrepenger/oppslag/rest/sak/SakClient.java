83c3c603df409be7549eef037ec77123277bfe78
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-08-25 18:20:12 +0200

    fjernet

--- src/main/java/no/nav/foreldrepenger/oppslag/rest/sak/SakClient.java
+++
@@ -1,9 +0,0 @@
-package no.nav.foreldrepenger.oppslag.rest.sak;
-
-import java.util.List;
-
-import no.nav.foreldrepenger.oppslag.ws.aktor.AktørId;
-
-public interface SakClient {
-    List<Sak> sakerFor(AktørId aktor, String tema);
-}
