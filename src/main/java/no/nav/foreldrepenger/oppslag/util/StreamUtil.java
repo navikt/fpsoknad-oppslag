48fe4cb4b2de44c868b4e45744b244da57b71bf0
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-08-31 21:04:40 +0200

    preview

--- src/main/java/no/nav/foreldrepenger/oppslag/util/StreamUtil.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/util/StreamUtil.java
@@ -1,11 +1,9 @@
 package no.nav.foreldrepenger.oppslag.util;
 
 import static java.util.Collections.emptyList;
-import static java.util.stream.Collectors.toList;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class StreamUtil {
@@ -14,13 +12,5 @@ public class StreamUtil {
 
     public static <T> Stream<T> safeStream(List<T> list) {
         return Optional.ofNullable(list).orElse(emptyList()).stream();
-    }
-
-    public static <T> List<T> distinct(List<T> list) {
-        return safeStream(list).distinct().collect(toList());
-    }
-
-    public static <T> Predicate<T> not(Predicate<T> t) {
-        return t.negate();
     }
 }
