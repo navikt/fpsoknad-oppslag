4d3b0340d1eef7a47fd9d5d857e5a0a339df27a0
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-03 19:18:19 +0200

    support 2 issuers

--- src/main/java/no/nav/foreldrepenger/oppslag/ws/bankkonto/BankkontoController.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/ws/bankkonto/BankkontoController.java
@@ -5,15 +5,11 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import no.nav.foreldrepenger.oppslag.http.ProtectedRestController;
 import no.nav.foreldrepenger.oppslag.util.TokenUtil;
-import no.nav.security.token.support.core.api.ProtectedWithClaims;
 
-@RestController
-@ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
-@RequestMapping(BankkontoController.PERSON)
+@ProtectedRestController(BankkontoController.PERSON)
 public class BankkontoController {
 
     private static final Logger LOG = LoggerFactory.getLogger(BankkontoController.class);
