13e77dbdf11d89ab5d0a52b0954d00086edc5fc0
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-03 13:45:22 +0200

    support multiple providers

--- src/main/java/no/nav/foreldrepenger/oppslag/config/Constants.java
+++ src/main/java/no/nav/foreldrepenger/oppslag/config/Constants.java
@@ -8,6 +8,7 @@ public final class Constants {
     public static final String NAV_AKTØR_ID = "Nav-Aktør-Id";
     public static final String X_CORRELATION_ID = "X-Correlation-ID";
     public static final String ISSUER = "selvbetjening";
+    public static final String TOKENX = "tokenx";
     public static final String INFOTRYGD = "IT01";
     public static final String FORELDREPENGER = "FOR";
     public static final String NAV_TOKEN_EXPIRY_ID = "Nav-Token-Expiry";
